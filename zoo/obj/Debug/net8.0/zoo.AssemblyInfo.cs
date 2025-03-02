d3498eeca33c40ef69f85d7152da40b4adfaff91
Якусевич Анна Ігорівна <ipz234_yaai@student.ztu.edu.ua>
2025-03-02 11:15:08 +0200

    Add modified files

--- zoo/obj/Debug/net8.0/zoo.AssemblyInfo.cs
+++ zoo/obj/Debug/net8.0/zoo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e4b91b58ab29202ce1be0b2eb71116ed8b4f77f")]
 [assembly: System.Reflection.AssemblyProductAttribute("zoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
